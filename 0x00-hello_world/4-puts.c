73b5ee4b4a8837ea87b558dc7958f8835de3f7b7
St-Pardon <okc4pardon@gmail.com>
2022-04-07 16:42:38 +0000

    task 4 fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
  * string printing funtion
- * main: The containing function of this programme
+ * main - The containing function of this programme
  * Return: this gives 0 if sucessful
  */
 int main(void)
