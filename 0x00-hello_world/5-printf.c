6b14fa5792d0385b672794dbb5a3bc185cf5bec0
Onyedikachi Onu <53058358+St-Pardon@users.noreply.github.com>
2022-04-07 18:31:07 +0100

    fixed task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase with proper grammar,
- * but the outcome is a piece of art,
- * Return: 0 if exited properly, non-zero otherwise
+ * main - the containing func
+ * Return: 0 if succesful
  */
 int main(void)
 {
