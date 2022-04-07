876db87d350762b17bf7329644fb72ee0fd7f957
St-Pardon <okc4pardon@gmail.com>
2022-04-07 16:34:33 +0000

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * string printing funtion
+ * main: The containing function of this programme
+ * Return: this gives 0 if sucessful
+ */
+int main(void)
+{
+puts("%s/n", "Programming is like building a multilingual puzzle");
+return (0);
+}
