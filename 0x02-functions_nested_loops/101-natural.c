39654b2fa0a2bbcb2a32f45d323680b4a448b33a
St-Pardon <okc4pardon@gmail.com>
2022-04-12 19:27:26 +0000

    debugged 13 advance

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,25 +7,24 @@
 
 int main(void)
 {
-	long int i, j, k, l;
+	unsigned long int sum3, sum5, sum;
+	int i;
 
-	j = 1;
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
 
-	k = 2;
-
-	for (i = 1; i <= 50; ++i)
+	for (i = 0; i < 1024; ++i)
 	{
-		if (j != 20365011074)
+		if ((i % 3) == 0)
 		{
-			printf("%ld, ", j);
-		} else
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
 		{
-			printf("%ld\n", j);
+			sum5 = sum5 + i;
 		}
-		l = j + k;
-		j = k;
-		k = l;
 	}
-
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
 	return (0);
 }
