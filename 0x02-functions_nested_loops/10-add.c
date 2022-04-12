0e9f68f0ee00fa8024cb5eecd371a4881f96ae69
St-Pardon <okc4pardon@gmail.com>
2022-04-12 18:25:59 +0000

    complested mandatory task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add nums
+ * @a: 1st parmeter
+ * @b: seond parameter
+ * Return: something
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - does something
+ * @n: parameter
+ */
+
+void print_to_98(int n)
+{
+	/* finally allowed to use stdio */
+	if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n > 98)
+	{
+		for (; n > 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("98\n");
+}
