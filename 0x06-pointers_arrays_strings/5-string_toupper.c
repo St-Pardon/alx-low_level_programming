0db18da76621e09801431a5f54a419c610ac8245
St-Pardon <okc4pardon@gmail.com>
2022-04-23 18:31:07 +0000

    task 6

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - to upper case
+ * @n: param
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\n'; i++)
+	{
+		if (n[i] > 96 && n[i] < 123)
+		{
+			n[i] = n[i] - 32;
+		}
+		else
+			n[i] = n[i];
+	}
+	return (n);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+char *string_toupper(char *);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
