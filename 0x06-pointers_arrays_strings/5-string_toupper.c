a232bca4eed50ae46ccb1ef73e8589ec2f15e8db
St-Pardon <okc4pardon@gmail.com>
2022-04-23 18:39:34 +0000

    task 5 3rd trial

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,14 +10,12 @@ char *string_toupper(char *n)
 {
 	int i;
 
-	for (i = 0; n[i] != '\n'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		if (n[i] > 96 && n[i] < 123)
 		{
 			n[i] = n[i] - 32;
 		}
-		else
-			n[i] = n[i];
 	}
 	return (n);
 }
