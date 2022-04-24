e76e1a7a0e5e356f8fb575d064b69ea5cd256bae
St-Pardon <okc4pardon@gmail.com>
2022-04-24 19:40:11 +0000

    advanced task

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *rot13(char *);
 char *leet(char *);
@@ -8,3 +10,4 @@ int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
+int _putchar(char c);
