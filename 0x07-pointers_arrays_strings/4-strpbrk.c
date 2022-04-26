5ec2096761127322048360b497ba9a9f37b1e167
St-Pardon <okc4pardon@gmail.com>
2022-04-26 17:46:30 +0000

    task 4 test 1

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches for string
+ * @s: poiter param
+ * @accept: poiter param
+ * Return: pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, c = 0;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				c++;
+				break;
+			}
+		}
+		if (c)
+		{
+			return (s);
+		}
+		else
+			s++;
+	}
+	if (*s == '\0')
+		return (s);
+	return (0);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
