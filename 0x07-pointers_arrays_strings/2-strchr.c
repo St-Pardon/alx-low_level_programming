9bda88c8bd18130b809643a61fb77f7b30e0cc9b
St-Pardon <okc4pardon@gmail.com>
2022-04-26 16:13:03 +0000

    task 3 test

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - find char
+ * @s: poiter params
+ * @c: params
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
