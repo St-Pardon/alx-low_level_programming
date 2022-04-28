84a28d5070f84c69d1f02a8fcd83855aa2a8db11
St-Pardon <okc4pardon@gmail.com>
2022-04-28 14:56:28 +0000

    task 1 test 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - reverse string
+ * @s: pointer param
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (!*s)
+	{
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
