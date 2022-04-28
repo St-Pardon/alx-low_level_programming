b7e81488627080a03094551ec5435cd421ff29b7
St-Pardon <okc4pardon@gmail.com>
2022-04-28 14:38:21 +0000

    task 0 test 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - recursive puts
+ * @s: pointer params
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
