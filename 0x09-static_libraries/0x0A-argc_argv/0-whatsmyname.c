270897d5d1997dcfb3597cf5f807cd786a830749
St-Pardon <okc4pardon@gmail.com>
2022-05-02 16:08:05 +0000

    task 0 test 1

---
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main func
+ * @argc: param count
+ * @argv: param pointer
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (argc);
+}
