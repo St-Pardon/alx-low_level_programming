21fd7ad7e488b9a93369f57dfb6ddb1f6185cc17
St-Pardon <okc4pardon@gmail.com>
2022-05-02 16:37:47 +0000

    task 3 test 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,12 +12,12 @@ int main(int argc, char *argv[])
 /*stdlib was included because of the "atio()" function*/
 /* the funct is used to covert str to int*/
 /* because the argv is an arrays of characters*/
-	if (argc < 1)
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
+}
 }
