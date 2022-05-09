459983c0ac8c72f2f2729d17b80853e769312ad5
St-Pardon <okc4pardon@gmail.com>
2022-05-09 22:08:17 +0000

    not working, try again tomorrow

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 46)
+			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{
 				printf("Error\n");
 				exit(98);
