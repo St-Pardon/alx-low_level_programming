e7f01254116cc9418518761127610e79a4d02069
St-Pardon <okc4pardon@gmail.com>
2022-05-09 19:53:40 +0000

    advanced task 101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+/**
+ * main - multiply 2 arg of num > 0
+ * @argc: param count
+ * @argv: param arr
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+
+	}
+
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+	return (0);
+}
