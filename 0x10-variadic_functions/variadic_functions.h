b745f315c968e828c5a4deb47fc86c0577ecb845
St-Pardon <okc4pardon@gmail.com>
2022-05-16 20:28:34 +0000

    task 2 - 3, test 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,8 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 
