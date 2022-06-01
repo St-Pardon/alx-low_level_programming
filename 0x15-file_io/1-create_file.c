586dbaa986b988f906166c1aab2027c22e181c17
St-Pardon <okc4pardon@gmail.com>
2022-06-01 19:38:40 +0000

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: param
+ * @text_content: 
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int of, nletters, rwr;
+
+	if (!filename)
+		return (-1);
+
+	of = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (of == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(of, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(of);
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
