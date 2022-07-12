b5158dcb6bff3fe87a79cc64e15a0eec60c580ad
St-Pardon <okc4pardon@gmail.com>
2022-07-12 17:49:13 +0000

    conrrect path

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/St-Pardon/alx-low_level_programming/raw/master/0x18-dynamic_libraries/my_inject.so
-export LD_PRELOAD=/tmp/my_inject.so
+wget -P /tmp https://github.com/St-Pardon/alx-low_level_programming/raw/master/0x18-dynamic_libraries/my-inject.so
+export LD_PRELOAD=/tmp/my-inject.so
