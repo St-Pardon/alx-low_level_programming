d1c66ef7443d5a0a04ed132160bb6a76e8f7e8bf
Onyedikachi Onu <53058358+St-Pardon@users.noreply.github.com>
2022-07-29 20:20:51 +0100

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -5,6 +5,9 @@
 #include <string.h>
 #include <stdio.h>
 
+#define TRUE 1
+#define FALSE 0
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -41,5 +44,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_delete(hash_table_t *ht);
 void hash_table_print(const hash_table_t *ht);
+
+char *str_copy(const char *s);
+char *str_copy1(const char *s);
+shash_table_t *shash_table_create(unsigned long int size);
+void insert_shash_node(shash_table_t *ht, shash_node_t *hn);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
 
 #endif
