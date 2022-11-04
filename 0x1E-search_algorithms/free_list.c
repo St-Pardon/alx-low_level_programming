1c3713891b3a6299b47a4d6d37aa8238b72aeb64
St-Pardon <okc4pardon@gmail.com>
2022-11-04 14:42:37 +0000

    :white_check_mark: completion

---
+++ 0x1E-search_algorithms/free_list.c
@@ -0,0 +1,18 @@
+#include "search_algos.h"
+
+/**
+ * free_list - Deallocates a singly linked list
+ * @list: Pointer to the linked list to be freed
+ */
+
+void free_list(listint_t *list)
+{
+	listint_t *node;
+
+	if (list)
+	{
+		node = list->next;
+		free(list);
+		free_list(node);
+	}
+}
